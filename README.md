9906e8e97408284a050e5249b33ed06dafc8c7ee
Your Name <you@example.com>
2025-05-30 22:40:05 +0900

    call backend

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # react-hello
+
+sample code for react+vine on frontend and python+fastapi on backend
+
+```
+docker-compose build
+docker-compose up
+```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/greet?name=ReactWTF")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
 
   return (
     <>
@@ -24,6 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p> { message || "Loading..." }</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
