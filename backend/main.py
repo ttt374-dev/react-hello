9e881ca255a2b34c9fd01f23060bfc0bc887ea93
Your Name <you@example.com>
2025-06-01 17:41:37 +0900

    greet working

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Allow frontend during dev
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,34 +8,24 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/greet?name=ReactWTF")
+    fetch("http://127.0.0.1:8000/api/greet?name=ReactWTF")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+      .then((data) => setMessage(data.message))
+        .catch((err) => {
+      console.error("Fetch error:", err);
+      setMessage(`Error: ${err.message}`);
+    });
   }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count} GREAT !!!! WTF
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
         <p> { message || "Loading..." }</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
