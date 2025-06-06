83cb82d0dd2e2263debf83f0f21f0b30d4e9f6b5
Your Name <you@example.com>
2025-06-06 15:57:08 +0900

    refactor

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,11 +2,34 @@ import { useParams } from 'react-router-dom';
 import List from "./List";
 import Transcript from "./Transcript";
 
+function HomeTest(){
+  const { title } = useParams();
+  return (
+    <div style={{ width: "100%", display: "flex", height: "100vh"}}>
+      <div style={{ width: "30%", backgroundColor: "lightgreen"}} selected={title}>asdfadf</div>
+      
+      <div style={{ width: "70%", backgroundColor: 'pink'}}>
+        <Transcript transcriptId={title}/>          
+      </div>
+    </div>
+  )
+}
+
+
+function Home2(){
+  return (
+    <div style="width: 100%; display: flex">
+      <div style="width: 30%; background-color: lightblue; ">Left</div>
+      <div style="flex: 1; background-color: lightyellow">Right</div>
+    </div>
+  )
+}
+
 function Home() {  
   const { title } = useParams();
 
-  return (
-    <div style={{ display: "flex", height: "100vh" }}>
+  return (    
+    <div style={{ width: "100%", display: "flex", height: "100vh" }}>
       <List selected={title}/>
       
       { title ? (
