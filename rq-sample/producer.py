fc1b61571046b9db74b7678032499f744332af2e
Your Name <you@example.com>
2025-06-07 10:34:46 +0900

    clean up

--- rq-sample/producer.py
+++
@@ -1,9 +0,0 @@
-import redis
-from rq import Queue
-from jobs import say_hello
-
-redis_conn = redis.from_url("redis://localhost:6379")
-q = Queue(connection=redis_conn)
-
-job = q.enqueue(say_hello, "WTF MEN!!!!")
-print(f"Job {job.id} enqueued")
